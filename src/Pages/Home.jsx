f313fec6915e2c558d77d42820e5a409b3348ac4
R-U-Bharti <kumar.bharti@gtropy.com>
2024-09-24 12:54:51 +0530

    changes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -62,6 +62,16 @@ const Home = () => {
           <h1 className='text-2xl font-medium text-center py-2 mt-2 mb-4 border-b border-gray-400 w-max px-10'>Collections By <span className="font-bold text-amber-400 hover:underline cursor-pointer" onClick={() => window.open('https://r-u-bharti.github.io/portfolio', '_blank')}>R U Bharti</span></h1>
         </div>
 
+        <div className="md:absolute top-4 right-4 flex items-center gap-2">
+          {decrypt(localPwd) === import.meta.env.VITE_PWD ?
+            <div onClick={() => sessionStorage.clear()} className='cursor-pointer text-xs text-green-400 border border-green-400 px-3 py-1 rounded hover:bg-green-600 hover:text-white'>Authenticated</div>
+            :
+            <>
+              <input type="password" onChange={e => setPwd(e.target.value)} value={pwd} name="" id="" className='border rounded px-3 text-sm py-1 bg-white/20 placeholder:text-white text-white focus:outline-none' placeholder='Enter password...' />
+              <button onClick={() => handleAuth()} className='text-xs text-red-400 border border-red-400 px-3 py-1 rounded hover:bg-red-600 hover:text-white'>Authenticate</button>
+            </>}
+        </div>
+
         {routes.map((card, index) => <>
           <div onClick={() => routeFun(card)} className="relative w-full md:w-[24%] transition-all duration-200 cursor-pointer border-2 flex flex-col items-center justify-between gap-2 p-4 rounded-md hover:border-blue-700 hover:shadow-[0px_0px_20px_rgba(0,0,255,0.5)] hover:bg-blue-800/10" key={index}>
             {card.path && decrypt(localPwd) !== import.meta.env.VITE_PWD && <div className='absolute top-0 right-0 bg-red-600 border border-red-400 px-3 py-1 text-xs rounded-bl'>Auth Required</div>}
@@ -79,15 +89,6 @@ const Home = () => {
         </>)}
       </div>
 
-      <div className="md:absolute top-4 right-4 flex items-center gap-2">
-        {decrypt(localPwd) === import.meta.env.VITE_PWD ?
-          <div onClick={() => sessionStorage.clear()} className='cursor-pointer text-xs text-green-400 border border-green-400 px-3 py-1 rounded hover:bg-green-600 hover:text-white'>Authenticated</div>
-          :
-          <>
-            <input type="password" onChange={e => setPwd(e.target.value)} value={pwd} name="" id="" className='border rounded px-3 text-sm py-1 bg-white/20 placeholder:text-white text-white focus:outline-none' placeholder='Enter password...' />
-            <button onClick={() => handleAuth()} className='text-xs text-red-400 border border-red-400 px-3 py-1 rounded hover:bg-red-600 hover:text-white'>Authenticate</button>
-          </>}
-      </div>
     </>
   )
 }
